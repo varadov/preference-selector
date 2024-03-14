c83d4b8e288563bec77a22e5f72ccfe66894bdd0
Juan Carlos Alvarado <jc.alvaradov93@gmail.com>
2024-03-14 03:06:12 -0300

    chore(prettier): add project formatting rules and format files

--- components/ui/category.tsx
+++ components/ui/category.tsx
@@ -1,56 +1,52 @@
-"use client";
+'use client'
 
-import { useState, ReactNode, Children, KeyboardEvent, MouseEvent } from "react";
-import { Badge } from "@/components/ui/badge";
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "@/components/ui/collapsible";
+import {useState, ReactNode, Children, KeyboardEvent, MouseEvent} from 'react'
+import {Badge} from '@/components/ui/badge'
+import {Collapsible, CollapsibleContent, CollapsibleTrigger} from '@/components/ui/collapsible'
 
 type TProps = {
-  name: string;
-  icon: ReactNode;
-  tabIndex: number;
-  children?: ReactNode | ReactNode[];
-};
+    name: string
+    icon?: ReactNode
+    tabIndex?: number
+    children?: ReactNode | ReactNode[]
+}
 
-export function Category({ name, icon, tabIndex, children }: TProps) {
-  const [isOpen, setIsOpen] = useState(false);
+export function Category({name, icon, tabIndex, children}: TProps) {
+    const [isOpen, setIsOpen] = useState(false)
 
-  const handleChildrenClick = (event: MouseEvent) => {
-    event.stopPropagation();
-  };
-
-  const handleChildrenKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
-    if (event.key === "Enter" || event.key === " ") {
-      event.preventDefault();
-      event.stopPropagation();
+    const handleChildrenClick = (event: MouseEvent) => {
+        event.stopPropagation()
     }
-  };
 
-  return (
-    <Collapsible open={isOpen} onOpenChange={setIsOpen} tabIndex={tabIndex}>
-      <CollapsibleTrigger>
-        <Badge
-          className="hover:cursor-pointer select-none text-nowrap h-7"
-          variant={isOpen ? "default" : "secondary"}
-        >
-          {icon}
-          {name}
-        </Badge>
-        <CollapsibleContent>
-          {Children.count(children) > 0 && (
-            <div
-              className="flex flex-wrap gap-2 pointer-events-auto"
-              onClick={handleChildrenClick}
-              onKeyDown={handleChildrenKeyDown}
-            >
-              {children}
-            </div>
-          )}
-        </CollapsibleContent>
-      </CollapsibleTrigger>
-    </Collapsible>
-  );
+    const handleChildrenKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
+        if (event.key === 'Enter' || event.key === ' ') {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+    }
+
+    return (
+        <Collapsible open={isOpen} onOpenChange={setIsOpen} tabIndex={tabIndex}>
+            <CollapsibleTrigger>
+                <Badge
+                    className="hover:cursor-pointer select-none text-nowrap h-7"
+                    variant={isOpen ? 'default' : 'secondary'}
+                >
+                    {icon}
+                    {name}
+                </Badge>
+                <CollapsibleContent>
+                    {Children.count(children) > 0 && (
+                        <div
+                            className="flex flex-wrap gap-2 pointer-events-auto"
+                            onClick={handleChildrenClick}
+                            onKeyDown={handleChildrenKeyDown}
+                        >
+                            {children}
+                        </div>
+                    )}
+                </CollapsibleContent>
+            </CollapsibleTrigger>
+        </Collapsible>
+    )
 }
